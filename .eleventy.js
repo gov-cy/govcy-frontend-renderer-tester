9b4f33720b4862bf153ef532deee2a65f92dcc23
Constantinos Evangelou <cevangelou@dits.dmrid.gov.cy>
2024-11-10 17:55:47 +0200

    Added support for site level sections rendering, such as header

--- .eleventy.js
+++ .eleventy.js
@@ -21,21 +21,44 @@ module.exports = function (eleventyConfig) {
             files.forEach(file => {
                 // read file
                 const siteData = JSON.parse(fs.readFileSync(path.join(__dirname, 'src', 'data', file), 'utf8')); // get site data
-                console.log('SiteData:', siteData);
+                // console.log('SiteData:', siteData);
                
                 // Inject script tag using Eleventy's url filter
                 const scriptTag = `
                     <script src="${pathPrefix}/js/route.js"></script>
                 `;
                 // set url and content for all pages in this file (or site)
-                const sitePages = siteData.pages.map(page => ({
-                    url: siteData.site.id + '/' + page.pageData.url,
-                    routes: siteData.routes,
-                    content: renderer.renderFromJSON(
-                        page.pageTemplate, 
-                        { "site": siteData.site, "pageData": page.pageData }
-                    ).replace('</body>', `<script>\n const DSFpathPrefix = '${pathPrefix}';\nconst DSFroutes=${JSON.stringify(siteData.routes)};</script>\n${scriptTag}\n</body>`)
-                }));
+                const sitePages = siteData.pages.map(page => {
+                    //---- handle site.sections (e.g. to have a site level header) ----
+                    // Check if siteData.sections exists
+                    if (siteData.site.sections) {
+                        // Iterate over each section in siteData.sections
+                        siteData.site.sections.forEach(siteSection => {
+                            // Check if this section is already present in page.pageTemplate.sections
+                            const hasSection = page.pageTemplate.sections.some(pageSection => pageSection.name === siteSection.name);
+    
+                            // If the section is missing, add it
+                            if (!hasSection) {
+                                page.pageTemplate.sections.push(siteSection);
+                            }
+                        });
+                    }
+                    return {
+                        url: siteData.site.id + '/' + page.pageData.url,
+                        routes: siteData.routes,
+                        content: renderer.renderFromJSON(
+                            page.pageTemplate, 
+                            { "site": siteData.site, "pageData": page.pageData }
+                        ).replace(
+                            '</body>', 
+                            `<script>
+                                const DSFpathPrefix = '${pathPrefix}';
+                                const DSFroutes=${JSON.stringify(siteData.routes)};
+                            </script>
+                            ${scriptTag}
+                            </body>`)
+                    }
+                });
                 // add file (or site) pages to collection array
                 pages = pages.concat(sitePages);
                 // console.log('Pages:', pages);
